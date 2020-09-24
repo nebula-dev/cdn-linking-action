5fa452b403b9d16674811d26948242fdd37646ca
Howard Stark <howard@getcoffee.io>
2020-09-24 16:30:29 -0400

    Add sed debug flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 echo $(pwd)
 
 find . -not -path '*/\.*' -not -path './static/*' -name '*.html' -exec \
-sed -i -e "s%\(href\|src\)\( *= *\)\"$TARGET_SRC\(.*\)\"%\1\2\"$CDN_BASE_URL$CDN_PATH\3\"%g" {} +
+sed --debug -i -e "s%\(href\|src\|content\)\( *= *\)\"$TARGET_SRC\(.*\)\"%\1\2\"$CDN_BASE_URL$CDN_PATH\3\"%g" {} +

97a17bed8a8d2d0ddf72231bc85a50654a96e927
Howard Stark <howard@getcoffee.io>
2020-09-24 16:25:53 -0400

    Add debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
 
+echo $(pwd)
+
 find . -not -path '*/\.*' -not -path './static/*' -name '*.html' -exec \
-sed -i -e "s%\(href\|src\)\( *= *\)\"$TARGET_SRC\(.*\)\"%\1\2\"$CDN_BASE_URL$CDN_PATH\3?v=$GITHUB_SHA\"%g" {} +
+sed -i -e "s%\(href\|src\)\( *= *\)\"$TARGET_SRC\(.*\)\"%\1\2\"$CDN_BASE_URL$CDN_PATH\3\"%g" {} +

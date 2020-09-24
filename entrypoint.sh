c86e7e7de870a719bbf442f7368089b8639f3256
Howard Stark <howard@getcoffee.io>
2020-09-24 16:34:13 -0400

    Add persistance test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,5 @@ echo $(pwd)
 
 find . -not -path '*/\.*' -not -path './static/*' -name '*.html' -exec \
 sed --debug -i -e "s%\(href\|src\|content\)\( *= *\)\"$TARGET_SRC\(.*\)\"%\1\2\"$CDN_BASE_URL$CDN_PATH\3\"%g" {} +
+
+echo "Persistance test" >> ./index.html

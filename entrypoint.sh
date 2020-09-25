ed8823c817f8186836efb769b1133ef7cf6ea2d7
Howard Stark <howard@getcoffee.io>
2020-09-25 12:42:10 -0400

    Remove debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,4 @@ set -e
 echo $(pwd)
 
 find . -not -path '*/\.*' -not -path './static/*' -name '*.html' -exec \
-sed --debug -i -e "s%\(href\|src\|content\)\( *= *\)\"$TARGET_SRC\(.*\)\"%\1\2\"$CDN_BASE_URL$CDN_PATH\3\"%g" {} +
-
-echo "Persistance test" >> ./index.html
+sed -i -e "s%\(href\|src\|content\)\( *= *\)\"$TARGET_SRC\(.*\)\"%\1\2\"$CDN_BASE_URL$CDN_PATH\3\"%g" {} +
